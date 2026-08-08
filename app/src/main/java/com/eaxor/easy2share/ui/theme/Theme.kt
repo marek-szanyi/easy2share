22bdc6b42d11bc662a6c0abd43e547cf93f5ceb6
Marek Szanyi <marek@memdump.sk>
2026-08-08 02:46:03 +0200

    Apply project-wide code formatting using Spotless and add MIT license

--- app/src/main/java/com/eaxor/easy2share/ui/theme/Theme.kt
+++ app/src/main/java/com/eaxor/easy2share/ui/theme/Theme.kt
@@ -5,7 +5,6 @@
  */
 package com.eaxor.easy2share.ui.theme
 
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
