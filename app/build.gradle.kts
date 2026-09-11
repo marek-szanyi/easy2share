e607ecc837fe7fbe2b4f974701ceae7792814f0b
Marek Szanyi <marek@memdump.sk>
2026-09-11 17:24:25 +0200

    Fix R8 rules used with release builds (#13)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -90,6 +90,7 @@ android {
             excludes += "META-INF/INDEX.LIST"
             excludes += "META-INF/io.netty.versions.properties"
             excludes += "META-INF/services/reactor.blockhound.integration.BlockHoundIntegration"
+            excludes += "META-INF/services/reactor.blockhound.integration.BlockHoundIntegration."
         }
     }
 }
